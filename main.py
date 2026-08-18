8146540f935fe5aea7263a76029b4a803a68a4e3
vignesh <95468303+vigneshvvv@users.noreply.github.com>
2026-08-18 12:03:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,5 +22,38 @@ def fetch_users():
     finally:
         closeConnection(connection, cursor)
 
+def fetchUserById(id,name):
+    connection = getConnection()
+
+    if connection is None:
+        print("Failed to connect ot DataBase")
+        return
+    cusror = connection.cursor(dictionary= True)
+    cusror.execute("select * from students where student_id = %s and student_name = %s", (id,name,))
+    data= cusror.fetchone()
+    print(data)
+    closeConnection
+    return data
+
+def insertData(data):
+    connection = getConnection()
+    if connection is None:
+            print("Failed to connect ot DataBase")
+            return
+
+    cursor = connection.cursor()
+    query = "INSERT INTO students VALUES (%s, %s, %s)"
+    values = (data["student_id"], data["student_name"], data["department"])
+    cursor.execute(query, values)
+    connection.commit()
+    print("Inserted data")
+    closeConnection(connection, cursor)
 
 fetch_users()
+# fetchUserById(2, "Arun")
+
+# insertData({
+#     "student_id": 6,
+#     "student_name": "Sathish",
+#     "department": "IT"
+# })
